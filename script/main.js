1ea584877ce9aad2a6624bbce30ac17e8e81487b
Ujval DR <72980673+UjvalDR@users.noreply.github.com>
2022-03-22 06:58:29 +0530

    yessss

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // Run when the webpage is loaded
 window.addEventListener('load', () => {
   Swal.fire({
-    title: 'Are you sure to play music in background?',
+    title: 'Do you want me to play music in background? :)',
     // text: "You won't be able to revert this!",
     icon: 'warning',
     showCancelButton: true,
